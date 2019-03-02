72f9b98b53c6ddffd368c9df7ec952a9a608fce0
James Pruegsanusak <korrawat@mit.edu>
2019-03-02 12:46:28 -0500

    Update image_processor.p.y, add processor_properties.py

--- image_processor.py
+++ image_processor.py
@@ -1,23 +1,44 @@
 import cv2
 import numpy as np
-from camera import Camera
+from processor_properties import ProcessorProperties
 
 
-class ImageProcessor:
-    def __init__(self, properties):
-        ''' properties is a dictionary of properties '''
-        self.properties = properties
+# class ImageStream:
+#     def __init__(self, camera, properties):
+#         ''' properties is a dictionary of properties '''
+#         self.camera = camera
+#         self.properties = properties
 
 
-    def updateProperties(self, properties):
-        self.properties = properties
+#     def read(self):
+#         pass
 
 
-    def change_contrast(self, image):
-        alpha = 2.2
-        beta=  50
-        return cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+def brightness(image, factor):
+    # if use `image * factor`, need to cast to int
+    # return image * factor
+    return cv2.convertScaleAbs(image, alpha=factor, beta=0)
 
 
-    def read(self):
-        pass
+def contrast(image, factor, midpoint=127):
+    ''' midpoint doesn't need to be exposed to user (too complicated) '''
+    # return midpoint + factor * (image - midpoint);
+    beta = midpoint * (1 - factor)
+    return cv2.convertScaleAbs(image, alpha=factor, beta=beta)
+
+def grayscale(image):
+    gray_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    return np.stack((gray_img,)*3, axis=-1)
+
+
+# TODO
+# - contrast increases noise! maybe add bilateral filter (slow)?
+# - gamma correction?
+
+def process_image(image, properties):
+    out = np.copy(image)
+    out = brightness(out, properties.brightness_factor.value)
+    out = contrast(out, properties.contrast_factor.value)
+    if properties.grayscale.value:
+        out = grayscale(out)
+    return out
